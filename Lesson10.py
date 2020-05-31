fe2e6078295f5b842379e0816e6f71883637be3e
max <maximnorby@gmail.com>
2020-06-01 00:05:37 +0300

    Lesson10.py done

--- Lesson10.py
+++ Lesson10.py
@@ -7,8 +7,13 @@ root.geometry('800x400+550+200')
 main_menu = Menu(root)
 root.config(menu=main_menu)
 
+
 # main_menu.add_command(label='File')
 # main_menu.add_command(label='About')
+
+def about_program():
+    pass
+
 
 # File
 file_menu = Menu(main_menu, tearoff=0)
@@ -17,6 +22,15 @@ file_menu.add_command(label='Save')
 file_menu.add_separator()
 file_menu.add_command(label='Exit')
 main_menu.add_cascade(label='File', menu=file_menu)
+
+# About
+help_menu = Menu(main_menu, tearoff=0)
+help_menu_sub = Menu(help_menu, tearoff=0)
+help_menu_sub.add_command(label='Online')
+help_menu_sub.add_command(label='Offline')
+help_menu.add_cascade(label='Help', menu=help_menu_sub)
+main_menu.add_cascade(label='FAQ', menu=help_menu)
+help_menu.add_command(label='About', command='about_program')
 
 # f_menu = Frame(root, bg='#1f252a', height=40)
 # f_menu.pack(fill=X)
